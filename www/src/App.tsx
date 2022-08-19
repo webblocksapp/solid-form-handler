3f613fe25a17c2952f011ef1ed095f7e082bae14
Mauricio Rivera <mauriver21@gmail.com>
2022-08-18 19:48:50 -0500

    fix: fixed scroll behavior when route change

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,7 +1,6 @@
 import { Component } from 'solid-js';
 import { useRoutes } from '@solidjs/router';
 import { mainRoutes } from '@routes';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const App: Component = () => {
   return useRoutes(mainRoutes);

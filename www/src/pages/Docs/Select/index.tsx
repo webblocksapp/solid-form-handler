20e97badd4b996fccbaf9cdea59055e0abf1dc7c
Mauricio Rivera <mauriver21@gmail.com>
2022-08-20 12:06:28 -0500

    update: added Select and TextInput docs.

--- www/src/pages/Docs/Select/index.tsx
+++ www/src/pages/Docs/Select/index.tsx
@@ -7,7 +7,33 @@ import { SelectCompForm } from '@implementations';
 export const Select: Component = () => (
   <DocsContentLayout>
     <h2 class="mb-4 border-bottom">Select</h2>
-    <p>Content</p>
+    <p>
+      You can take a look at the component definition <code>Select.tsx</code>{' '}
+      which is inside the code tab. Here will inherit the{' '}
+      <code>SelectProps</code> from the <code>HTMLSelectElement</code> to avoid
+      redefining all the props of a vanilla HTML select (open closed principle).
+      If you will use a <i>SolidJS</i> UI library that provides a predefined{' '}
+      <code>Select</code> component, the same approach can be done by extending
+      the <code>SelectProps</code> from the <code>UILibrarySelectProps</code>{' '}
+      interface.
+    </p>
+    <p>
+      At <code>SelectProps</code> the <code>formHandler</code> is defined as an
+      optional prop to preserve the original nature of a select (open closed
+      principle) which is not dependent on a <code>formHandler</code> prop. The{' '}
+      <code>onInput</code> and <code>onBlur</code> events were extended by the
+      methods <code>onInput</code> and <code>onBlur</code> which are defined
+      inside the component. Those methods implement the <code>formHandler</code>{' '}
+      and also preserve the <code>onInput</code> and <code>onBlur</code> props
+      execution when they are defined.
+    </p>
+    <p>
+      As a result, we will have a shorter implementation at{' '}
+      <code>Form.tsx</code> by only passing the field <code>name</code>,{' '}
+      <code>options</code> and the <code>formHandler</code> object to the{' '}
+      <code>&lt;Select /&gt;</code> component for handling form data
+      validations.
+    </p>
     <Implementation
       codeTabs={[
         { name: 'Form.tsx', code: getRaw('SelectCompForm') },
--- www/src/pages/Docs/TextInput/index.tsx
+++ www/src/pages/Docs/TextInput/index.tsx
@@ -7,7 +7,32 @@ import { getRaw } from '@utils';
 export const TextInput: Component = () => (
   <DocsContentLayout>
     <h2 class="mb-4 border-bottom">TextInput</h2>
-    <p>Content</p>
+    <p>
+      You can take a look at the component definition <code>TextInput.tsx</code>{' '}
+      which is inside the code tab. Here will extend the{' '}
+      <code>TextInputProps</code> from the <code>HTMLInputElement</code> to
+      avoid redefining all the props of a vanilla HTML input (open closed
+      principle). If you will use a <i>SolidJS</i> UI library that provides a
+      predefined <code>TextInput</code> component, the same approach can be done
+      by extending the <code>TextInputProps</code> from the{' '}
+      <code>UILibraryTextInputProps</code> interface.
+    </p>
+    <p>
+      At <code>TextInputProps</code> the <code>formHandler</code> is defined as
+      an optional prop to preserve the original nature of a text input (open
+      closed principle) which is not dependent on a <code>formHandler</code>{' '}
+      prop. The <code>onInput</code> and <code>onBlur</code> events were
+      extended by the methods <code>onInput</code> and <code>onBlur</code> which
+      are defined inside the component. Those methods implement the{' '}
+      <code>formHandler</code> keeping the <code>onInput</code> and{' '}
+      <code>onBlur</code> props execution when they are defined.
+    </p>
+    <p>
+      As a result, we will have a shorter implementation at{' '}
+      <code>Form.tsx</code> by only passing the field <code>name</code> and the{' '}
+      <code>formHandler</code> object to the <code>&lt;TextInput /&gt;</code>{' '}
+      component for handling form data validations.
+    </p>
     <Implementation
       codeTabs={[
         { name: 'Form.tsx', code: getRaw('TextInputCompForm') },

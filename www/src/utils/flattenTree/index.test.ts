ad3d8bdbe6dd79cad0086b14a2bb0d2df717b30b
Mauricio Rivera <mauriver21@gmail.com>
2022-09-28 15:47:35 -0500

    Fixed www test.

--- www/src/utils/flattenTree/index.test.ts
+++ www/src/utils/flattenTree/index.test.ts
@@ -9,16 +9,12 @@ describe('flattenTree', () => {
       {
         text: 'Getting started',
         section: true,
-        __id: '1',
-        __level: 1,
       },
-      { text: 'Introduction', route: 'introduction', __id: '1-1', __level: 2 },
+      { text: 'Introduction', route: 'introduction' },
       {
         text: 'Validations',
         route: 'validations',
         section: true,
-        __id: '2',
-        __level: 1,
       },
     ]);
   });

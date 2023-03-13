913c79471d1ddc7808208a2bceb45525cc724860
Mauricio Rivera <mauriver21@gmail.com>
2023-03-13 13:22:07 -0500

    Updated checkboxes docs.

--- www/src/pages/Docs/Checkboxes/index.tsx
+++ www/src/pages/Docs/Checkboxes/index.tsx
@@ -1,5 +1,5 @@
 import { Component } from 'solid-js';
-import { Implementation, Tabs } from '@components';
+import { Code, Implementation, Tabs } from '@components';
 import { getRaw } from '@utils';
 import { YupCheckboxesCompForm, ZodCheckboxesCompForm } from '@implementations';
 
@@ -7,47 +7,30 @@ export const Checkboxes: Component = () => (
   <>
     <h2 class="mb-4 border-bottom">Checkboxes</h2>
     <p>
-      You can take a look at the component definition{' '}
-      <code>Checkboxes.tsx</code> which is inside the code tab. The Checkboxes
-      or CheckboxGroup is a custom component that uses under the hood the
-      previously created Checkbox component. If you will use a <i>SolidJS</i> UI
-      library that provides a predefined Checkboxes component, you can build it
-      by extending the <code>CheckboxesProps</code> from the{' '}
-      <code>UILibraryCheckboxesProps</code> interface instead of creating the
-      component from scratch.
+      You can build your own <code>&lt;Checkboxes /&gt;</code> component around
+      the form handler <code>&lt;Field /&gt;</code> component in{' '}
+      <code>checkbox-group</code> mode. No native html checkbox group component
+      exists so you need to define the signature from scratch, unless you are
+      building from any <i>SolidJS</i> ui library, you may extend from the
+      respective <code>UILibraryCheckboxGroupProps</code> signature. Finally,
+      you need to make use of the <code>FieldProps</code> signature which
+      contains the needed props for the <code>&lt;Field /&gt;</code> component.
     </p>
     <p>
-      At <code>CheckboxesProps</code> the <code>formHandler</code> is defined as
-      an optional prop to preserve the original nature of a group of checkboxes
-      that are not dependent on a <code>formHandler</code> prop. The{' '}
-      <code>onChange</code> event was extended by the method{' '}
-      <code>onChange</code> which is defined inside the component. This method
-      implements the <code>formHandler</code> and also preserves the{' '}
-      <code>onChange</code> prop execution when it's defined. Additionally, it
-      contains the logic for handling the checkboxes value as an array of{' '}
-      <code>number | string</code> primitives.
+      Also, you can notice that the <code>&lt;Checkboxes /&gt;</code> is built
+      from the <code>&lt;Checkbox /&gt;</code> component created previously.
     </p>
+    <Code content={getRaw('components/Checkboxes')} />
     <p>
-      As a result, we will have a shorter implementation at{' '}
-      <code>Form.tsx</code> by only passing the <code>name</code>,{' '}
-      <code>options</code> and the <code>formHandler</code> object to the{' '}
-      <code>&lt;Checkboxes /&gt;</code> component for handling form data
-      validations.
+      The following is an implementation of the built{' '}
+      <code>&lt;Checkboxes /&gt;</code> component.
     </p>
     <Tabs
       tabs={[
         {
           text: 'yup',
           children: (
-            <Implementation
-              codeTabs={[
-                { name: 'Form.tsx', code: getRaw('CheckboxesCompForm/yup') },
-                {
-                  name: 'Checkboxes.tsx',
-                  code: getRaw('components/Checkboxes'),
-                },
-              ]}
-            >
+            <Implementation code={getRaw('CheckboxesCompForm/yup')}>
               <YupCheckboxesCompForm />
             </Implementation>
           ),
@@ -55,15 +38,7 @@ export const Checkboxes: Component = () => (
         {
           text: 'zod',
           children: (
-            <Implementation
-              codeTabs={[
-                { name: 'Form.tsx', code: getRaw('CheckboxesCompForm/zod') },
-                {
-                  name: 'Checkboxes.tsx',
-                  code: getRaw('components/Checkboxes'),
-                },
-              ]}
-            >
+            <Implementation code={getRaw('CheckboxesCompForm/zod')}>
               <ZodCheckboxesCompForm />
             </Implementation>
           ),

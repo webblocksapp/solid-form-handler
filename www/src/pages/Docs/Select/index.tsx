2e8afdc1768761136da308a36ca98a06fb84c1c2
Mauricio Rivera <mauriver21@gmail.com>
2023-03-13 12:40:01 -0500

    Updated select component docs.

--- www/src/pages/Docs/Select/index.tsx
+++ www/src/pages/Docs/Select/index.tsx
@@ -1,5 +1,5 @@
 import { Component } from 'solid-js';
-import { Implementation, Tabs } from '@components';
+import { Code, Implementation, Tabs } from '@components';
 import { getRaw } from '@utils';
 import { YupSelectCompForm, ZodSelectCompForm } from '@implementations';
 
@@ -7,43 +7,27 @@ export const Select: Component = () => (
   <>
     <h2 class="mb-4 border-bottom">Select</h2>
     <p>
-      You can take a look at the component definition <code>Select.tsx</code>{' '}
-      which is inside the code tab. Here will inherit the{' '}
-      <code>SelectProps</code> from the <code>HTMLSelectElement</code> to avoid
-      redefining all the props of a vanilla HTML select. If you will use a{' '}
-      <i>SolidJS</i> UI library that provides a predefined <code>Select</code>{' '}
-      component, the same approach can be done by extending the{' '}
-      <code>SelectProps</code> from the <code>UILibrarySelectProps</code>{' '}
-      interface.
+      You can build your own <code>&lt;Select /&gt;</code> component around the
+      form handler <code>&lt;Field /&gt;</code> component in <code>input</code>{' '}
+      mode. To avoid redefining the select props, you can extend from the{' '}
+      <code>HTMLSelectElement</code> signature if you are building from a native
+      html select. In case you are building from any <i>SolidJS</i> ui library,
+      you may extend from the respective <code>UILibrarySelectProps</code>{' '}
+      signature. Finally, you need to make use of the <code>FieldProps</code>{' '}
+      signature which contains the needed props for the{' '}
+      <code>&lt;Field /&gt;</code> component.
     </p>
+    <Code content={getRaw('components/Select')} />
     <p>
-      At <code>SelectProps</code> the <code>formHandler</code> is defined as an
-      optional prop to preserve the original nature of a select which is not
-      dependent on a <code>formHandler</code> prop. The <code>onInput</code> and{' '}
-      <code>onBlur</code> events were extended by the methods{' '}
-      <code>onInput</code> and <code>onBlur</code> which are defined inside the
-      component. Those methods implement the <code>formHandler</code> and also
-      preserve the <code>onInput</code> and <code>onBlur</code> props execution
-      when they are defined.
-    </p>
-    <p>
-      As a result, we will have a shorter implementation at{' '}
-      <code>Form.tsx</code> by only passing the <code>name</code>,{' '}
-      <code>options</code> and the <code>formHandler</code> object to the{' '}
-      <code>&lt;Select /&gt;</code> component for handling form data
-      validations.
+      The following is an implementation of the built{' '}
+      <code>&lt;Select /&gt;</code> component.
     </p>
     <Tabs
       tabs={[
         {
           text: 'yup',
           children: (
-            <Implementation
-              codeTabs={[
-                { name: 'Form.tsx', code: getRaw('SelectCompForm/yup') },
-                { name: 'Select.tsx', code: getRaw('components/Select') },
-              ]}
-            >
+            <Implementation code={getRaw('SelectCompForm/yup')}>
               <YupSelectCompForm />
             </Implementation>
           ),
@@ -51,12 +35,7 @@ export const Select: Component = () => (
         {
           text: 'zod',
           children: (
-            <Implementation
-              codeTabs={[
-                { name: 'Form.tsx', code: getRaw('SelectCompForm/zod') },
-                { name: 'Select.tsx', code: getRaw('components/Select') },
-              ]}
-            >
+            <Implementation code={getRaw('SelectCompForm/zod')}>
               <ZodSelectCompForm />
             </Implementation>
           ),

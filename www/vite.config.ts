bfbe4a0f649ac937ac16e617e5ecac177fb4b9b7
Mauricio Rivera <mauriver21@gmail.com>
2023-04-29 00:34:52 -0500

    Changed vite static files copy flow.

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -1,10 +1,10 @@
 import { createHtmlPlugin } from 'vite-plugin-html';
 import { defineConfig } from 'vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import copy from 'rollup-plugin-copy';
 import solidPlugin from 'vite-plugin-solid';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     plugins: [
       solidPlugin(),
@@ -17,19 +17,34 @@ export default defineConfig(({ mode }) => {
           },
         },
       }),
-      viteStaticCopy({
-        targets: [
-          {
-            src: 'src/assets/images/solid-form-handler.png',
-            dest: 'images',
-          },
-        ],
-      }),
     ],
     server: { port: 4000 },
     build: {
       sourcemap: true,
       target: 'esnext',
+      rollupOptions: {
+        plugins: [
+          copy({
+            copyOnce: true,
+            hook: 'closeBundle',
+            verbose: true,
+            targets: [
+              {
+                src: 'src/assets/images/**/*',
+                dest: 'dist/images',
+              },
+            ],
+          }),
+        ],
+      },
+    },
+    resolve: {
+      alias: {
+        'solid-form-handler/yup':
+          '../packages/lib/dist/adapters/yupSchema/index.es.js',
+        'solid-form-handler/zod':
+          '../packages/lib/dist/adapters/zodSchema/index.es.js',
+      },
     },
   };
 });

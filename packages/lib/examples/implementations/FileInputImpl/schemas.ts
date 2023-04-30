072c41f372d2ebf2f69a1c7973adf637fb4482de
Mauricio Rivera <mauriver21@gmail.com>
2023-04-30 16:02:36 -0500

    Changed file schemas defs.

--- packages/lib/examples/implementations/FileInputImpl/schemas.ts
+++ packages/lib/examples/implementations/FileInputImpl/schemas.ts
@@ -2,20 +2,10 @@ import * as yup from 'yup';
 import { yupSchema, zodSchema } from '@adapters';
 import { z } from 'zod';
 
-const fileIsRequired = (value?: File) => (value ? true : false);
+const isRequired = (value?: File) => (value ? true : false);
 const fileSize = (value?: File) => {
   const size = value?.size || 0;
   return size <= 200000 ? true : false;
-};
-const filesAreRequired = (value?: File[]) => {
-  const numberOfFiles = value?.length || 0;
-  return numberOfFiles >= 2 ? true : false;
-};
-const fileSizes = (value?: File[]) => {
-  value?.forEach?.((item) => {
-    return fileSize(item);
-  });
-  return true;
 };
 
 const _ySchema: yup.Schema<{
@@ -24,30 +14,26 @@ const _ySchema: yup.Schema<{
 }> = yup.object().shape({
   document: yup
     .mixed<File>()
-    .test({
-      name: 'fileIsRequired',
-      message: 'File is required',
-      test: fileIsRequired,
-    })
+    .required()
     .test({
       name: 'fileSize',
       message: 'File exceeds 200kb',
       test: fileSize,
     })
-    .required(),
+    .test({ name: 'isRequired', message: 'File is required', test: isRequired }),
   documents: yup
-    .mixed<File[]>()
-    .test({
-      name: 'filesAreRequired',
-      message: 'Min 2 files are required',
-      test: filesAreRequired,
-    })
-    .test({
-      name: 'fileSizes',
-      message: 'One File exceeds 200kb',
-      test: fileSizes,
-    })
-    .required(),
+    .array(
+      yup
+        .mixed<File>()
+        .test({
+          name: 'fileSizes',
+          message: 'One File exceeds 200kb',
+          test: fileSize,
+        })
+        .required()
+    )
+    .required()
+    .min(2),
 });
 
 export const ySchema = yupSchema(_ySchema);
@@ -55,11 +41,15 @@ export const zSchema = zodSchema(
   z.object({
     document: z
       .custom<File>()
-      .refine(fileIsRequired, { message: 'File is required' })
+      .refine(isRequired, { message: 'File is required' })
       .refine(fileSize, { message: 'File exceeds 100kb' }),
     documents: z
-      .custom<File[]>()
-      .refine(filesAreRequired, { message: 'Min 2 files are required' })
-      .refine(fileSizes, { message: 'One File exceeds 200kb' }),
+      .array(
+        z
+          .custom<File>()
+          .refine(isRequired, { message: 'File is required' })
+          .refine(fileSize, { message: 'File exceeds 100kb' })
+      )
+      .min(2),
   })
 );
